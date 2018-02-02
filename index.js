4e5ade92b5af82fb8be07d839dedfd3130009207
asday <sara.and.zuka@gmail.com>
2018-02-02 12:33:27 +0000

    Added code from stack overflow.

---
+++ index.js
@@ -0,0 +1,14 @@
+const { OrderedMap } = require("immutable");
+
+let test = OrderedMap({
+	a: 10,
+	b: 20,
+	c: 30,
+	d: 40,
+});
+
+console.log(test);
+
+test = test.delete('b');
+
+console.log(test);
